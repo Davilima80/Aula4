7b0965bd24f253d56e8d051f1b059b3e6b7dc95b
Davilima80 <davilimahonoratofernandeslima@gmail.com>
2024-05-15 15:28:24 -0300

    Add files via upload

---
+++ aula4.py
@@ -0,0 +1,36 @@
+#1
+print("Olá")
+#2
+booleana = True
+print(type(booleana))
+#3
+print(1.2*3.4)
+#4
+print(4/2)
+#5
+print(8-4)
+#6
+print(10//2)
+#7
+print(3.5*2.9)
+#8
+num=10
+print(num**2)
+#9
+#numero1 = (input("Dgite um número: "))
+#numero2 = (input("Dgite outro número: "))
+
+#print(f'''
+#Resultado da soma: {numero1 + numero2}
+#Resultado da subtração: {numero1 - numero2}
+#Resultado da multiplicação: {numero1 * numero2}
+#Resultado da divisão: {numero1 // numero2}
+#'''
+#)
+
+nome = (input("Digite seu nome: "))
+email = (input("Digite seu email: "))
+idade = (input("Digite sua idade: "))
+altura = (input("Digite sua altura: "))
+
+print("olá", nome, "sua idade e altura são", idade, altura)
